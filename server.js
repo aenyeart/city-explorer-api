f5528129165d3f4eb22e8833950e8e6f9ab279a9
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-12-08 01:03:00 -0800

    Forecast array in progress

--- server.js
+++ server.js
@@ -14,23 +14,30 @@ app.get('/weather', handleGetWeather);
 
 class Forecast {
   constructor(dataObj) {
+    this.description = `Low of ${dataObj.low_temp}, high of ${dataObj.high_temp} with ${dataObj.weather.description.toLowerCase()}`;
     this.date = dataObj.valid_date;
-    this.description = dataObj.weather.description;
   }
-};
+}
 
 function handleGetWeather(req, res) {
 
   let lat = req.query.lat;
   let lon = req.query.lon;
   let searchQuery = req.query.searchQuery;
+
   let cityWeatherData = weatherData.find((elem) => {
     return elem.city_name === searchQuery;
   });
-  console.log(cityWeatherData);
+
+  const forecastArr = cityWeatherData.data.map((dataObj) => {
+    Forecast.constructor(dataObj);
+  });
+
+  // console.log(cityWeatherData.data[0]);
+
   res.send(
     cityWeatherData ?
-      `the timezone for ${cityWeatherData.city_name} is ${cityWeatherData.timezone}` :
+      (`the timezone for ${cityWeatherData.city_name} is ${cityWeatherData.timezone}`, forecastArr) :
       `ERROR: Please try searching for "Seattle", "Paris", or "Amman" instead.`);
   // console.log(`lat: ${lat}, lon: ${lon}, searchQuery: ${searchQuery}`);
 }
