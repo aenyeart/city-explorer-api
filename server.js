8259cfee0f7f8f3807ee681610e91c97816be88f
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-12-07 18:52:20 -0800

    Sets up Express server

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const PORT = 3001;
+
+app.listen(PORT, () => console.log('server is listening at port', PORT));
