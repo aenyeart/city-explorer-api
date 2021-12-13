e98fa1dd7391ff9b380b0c3598a07ebb1d6c49c3
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-12-13 08:54:43 -0800

    Begins implementing cache

--- route-handlers/weather.js
+++ route-handlers/weather.js
@@ -1,5 +1,7 @@
 'use strict';
 const axios = require('axios');
+let cache = require('./cache.js');
+
 
 class Forecast {
   constructor(dailyWeather) {
@@ -12,13 +14,24 @@ async function handleGetWeather(req, res) {
   let lat = req.query.lat;
   let lon = req.query.lon;
   let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHERBIT_API_KEY}&units=I&lat=${lat}&lon=${lon}`; // gets ~10 days of weather
-  console.log(url);
+  const key = `weather-${lat}${lon}`;
+
   try {
-    let weatherData = await axios.get(url);
-    console.log(weatherData.data.data);
-    let forecastArray = weatherData.data.data.map((dailyWeather) => new Forecast(dailyWeather));
-    console.log(forecastArray);
-    res.status(200).send(forecastArray);
+
+    if (cache[key]) console.log('Cache hit');
+    else {
+      console.log('Cache miss');
+      cache[key] = {};
+      cache[key].timestamp = Date.now();
+      // cache[key].data =
+
+
+      let weatherData = await axios.get(url);
+
+      let forecastArray = weatherData.data.data.map((dailyWeather) => new Forecast(dailyWeather));
+      console.log(forecastArray);
+      res.status(200).send(forecastArray);
+    }
   } catch (e) {
     res.status(500).send(`There was an error in fetching weather data`);
   }
